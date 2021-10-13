80b334e51f8230b5c4cc7418276dfcbee266d6c9
Innnsane <1355616277@qq.com>
2021-10-13 17:33:27 +0800

    fork

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-# %%
 import ujson
-import csv
 from utils import *
 import itertools
 import math
@@ -8,147 +6,177 @@ import pulp as lp
 
 # %% 战区关卡参数
 # 枪种系数：
-coef ={'HG':11,'SMG':10,'RF':6,'AR':11,'MG':3,'SG':5}
+TYPE = ["", "HG", "SMG", "RF", "AR", "MG", "SG"]
+COEF = {'HG': 1, 'SMG': 1, 'RF': 1, 'AR': 1, 'MG': 1, 'SG': 1}
 # 妖精加成：5星1.25
-fairy_ratio = 1.25
+FAIRY = 1.25
 # 优势人形
-superior = ['SIG MCX', "AUG SMG","蜜獾","Model L","菲德洛夫"]
+ADVANTAGE = []
 # 昼夜战
-fight_mode = 'day'
-max_dolls = 20
+ENVIRONMENT = 'night'
+# 上场人形数量
+MAX_DOLL = 30
 # %%
-name_table = get_name_table()
-# %%
-with open(r'resource/doll.json') as f:
-    doll_info = ujson.load(f)
-with open(r'resource/equip.json',encoding='utf-8') as f:
-    equip_info = ujson.load(f)
-with open(r'info/user_info.json') as f:
-    user_info = ujson.load(f)
-# %% 统计持有人形信息
-my_dolls = {}
-for doll in doll_info:
-    id = doll['id']
-    if 1200 < id < 20000 or id > 30000:
-        continue
-    my_dolls[doll['id']] = {
-        'id':doll['id'],
-        'name':name_table[doll['name']],
-        'gun_level': 0,
-        'skill1': 1,
-        'skill2': 0,
-        'number': 1,
-        'favor': 0,
-    }
-for doll in user_info['gun_with_user_info']:
-    for k in ['gun_level','skill1','skill2','number']:
-        my_dolls[int(doll['gun_id'])][k] = max(int(doll[k]), my_dolls[int(doll['gun_id'])][k])
-    my_dolls[int(doll['gun_id'])]['favor'] = max(int(doll['favor'])//10000, my_dolls[int(doll['gun_id'])]['favor'])
 
-with open(r'info/my_dolls.json','w',encoding='utf-8') as f:
-    ujson.dump(my_dolls, f, ensure_ascii=False, indent=2)
-# %% 统计持有装备信息
-my_equips = {}
-for equip in equip_info:
-    if equip['rank'] < 5:
-        continue
-    id = equip['id']
-    my_equips[equip['id']] = {
-        'id':equip['id'],
-        'name':name_table[equip['name']],
-        'code':equip['code'],
-        'fit_guns': equip['fit_guns'],
-        'level_00': 0,
-        'level_10': 0,
-    }
-for _, equip in user_info['equip_with_user_info'].items():
-    id = int(equip['equip_id'])
-    if id not in my_equips.keys():
-        continue
-    level = int(equip['equip_level'])
-    if level == 10:
-        my_equips[id]['level_10'] += 1
-    else:
-        my_equips[id]['level_00'] += 1
-        
-with open(r'info/my_equips.json','w',encoding='utf-8') as f:
-    ujson.dump(my_equips, f, ensure_ascii=False, indent=2)
 
-# %% 计算各人形不同配装的效能
-choices = {}
-for doll in doll_info:
-    id = doll['id']
-    if 1200 < id < 20000 or id > 30000:
-        continue
-    if my_dolls[id]['gun_level'] == 0:
-        continue
-    equip_group_all = []
-    for category, type_str in enumerate(doll['type_equip'].split('|')):
-        equip_group_category = []
-        types = [int(i) for i in type_str.split(',')]
-        # print(category, types)
-        for equip in equip_info:
-            if equip['rank'] <5 or equip['type'] not in types:
-                continue
-            if equip['fit_guns'] and id not in equip['fit_guns']:
-                continue
-            eid = equip['id']
-            if my_equips[eid]['level_10'] > 0:
-                equip_group_category.append((equip, 10))
-            if my_equips[eid]['level_00'] > 0 and my_equips[eid]['level_10'] < max_dolls:
-                equip_group_category.append((equip, 0))
-        # print([name_table[equip[0]['name']] for equip in equip_group_category])
-        equip_group_all.append(equip_group_category)
-    # print(my_dolls[id])
-    # print(doll)
-    for i,j,k in itertools.product(*equip_group_all):
-        strength = doll_attr_calculate(doll,my_dolls[id],[i,j,k])
-        # print(strength)
-        sp_ratio = 1.2 if my_dolls[id]['name'] in superior else 1
-        score = math.floor(coef[doll['type']]*sp_ratio*fairy_ratio*strength[fight_mode]/100)
-        # print(name_table[i[0]['name']],i[1],name_table[j[0]['name']],j[1],name_table[k[0]['name']],k[1],score)
-        recipe_name = f"{my_dolls[id]['name']}_{name_table[i[0]['name']]}_{i[1]}_{name_table[j[0]['name']]}_{j[1]}_{name_table[k[0]['name']]}_{k[1]}"
-        recipe_content = {
-            my_dolls[id]['name']:-1,
-            f"{name_table[i[0]['name']]}_{i[1]}":0,
-            f"{name_table[j[0]['name']]}_{j[1]}":0,
-            f"{name_table[k[0]['name']]}_{k[1]}":0,
-            'score': score
+def theater_area_setting(theater_area_id):
+    with open(r'resource/theater_area_info.json') as f_theater_area:
+        theater_area_info = ujson.load(f_theater_area)
+        f_theater_area.close()
+
+    for area in theater_area_info:
+        if area["id"] == theater_area_id:
+            COEF['HG'] = area["boss_score_coef"].split(";")[0]
+            COEF['SMG'] = area["boss_score_coef"].split(";")[1]
+            COEF['RF'] = area["boss_score_coef"].split(";")[2]
+            COEF['AR'] = area["boss_score_coef"].split(";")[3]
+            COEF['MG'] = area["boss_score_coef"].split(";")[4]
+            COEF['SG'] = area["boss_score_coef"].split(";")[5]
+            MAX_DOLL = 5 + 10 + int(area["theater_spare_gun_num"])
+            ADVANTAGE = area["advantage_gun"].split(",")
+
+
+def user_info_handle():
+    user_info = open_json(r'info/user_info.json')
+
+    gun_text = open_text(r'resource/gun.txt')
+    equip_text = open_text(r'resource/equip.txt')
+    gun_info = open_json(r'resource/gun_info.json', "utf-8")
+    equip_info = open_json(r'resource/equip_info.json', "utf-8")
+
+    # %% 统计持有人形信息
+    my_dolls = {}
+    for gun in gun_info:
+        doll_id = int(gun['id'])
+        if 1200 < doll_id < 20000 or doll_id > 30000:
+            continue
+        my_dolls[gun['id']] = {
+            'id': gun['id'],
+            'name': stc_to_text(gun_text, gun["name"]),
+            'gun_level': 0,
+            'skill1': 1,
+            'skill2': 0,
+            'number': 1,
+            'favor': 0,
         }
-        recipe_content[f"{name_table[i[0]['name']]}_{i[1]}"]-=1
-        recipe_content[f"{name_table[j[0]['name']]}_{j[1]}"]-=1
-        recipe_content[f"{name_table[k[0]['name']]}_{k[1]}"]-=1
-        choices[recipe_name] = recipe_content
-print(len(choices.items()))
-# %%
-resource = {}
-for id, doll in my_dolls.items():
-    if doll['gun_level'] > 0:
-        resource[doll['name']]=1
-for id, equip in my_equips.items():
-    if equip['level_10'] > 0:
-        resource[f"{equip['name']}_10"]=equip['level_10']
-    if equip['level_00'] > 0:
-        resource[f"{equip['name']}_0"]=equip['level_00']
-resource['count'] = max_dolls
-resource['score'] = 0
-lp_vars = {}
-problem = lp.LpProblem('battlefield', lp.LpMaximize)
-for k, recipe in choices.items():
-    lp_vars[k] = lp.LpVariable(k, cat=lp.LpBinary)
-    for r, c in recipe.items():
-        resource[r] += c*lp_vars[k]
-    resource['count'] -= lp_vars[k]
-for k, v in resource.items():
-    problem += v >= 0, k
-problem += resource['score']
-print(problem.solve())
-print(resource['score'].value())
-res = []
-for k, v in lp_vars.items():
-    if v.value()>0:
-        res.append((k, choices[k]['score'], v.value()))
-res.sort(key=lambda x: x[1], reverse=True)
-for r in res:
-    print(r[0], r[1], r[2])
-# %%
+
+    for doll in user_info['gun_with_user_info']:
+        for k in ['gun_level', 'skill1', 'skill2', 'number']:
+            my_dolls[doll['gun_id']][k] = max(int(doll[k]), my_dolls[doll['gun_id']][k])
+        my_dolls[doll['gun_id']]['favor'] = max(int(doll['favor']) // 10000, my_dolls[doll['gun_id']]['favor'])
+
+    # %% 统计持有装备信息
+    my_equips = {}
+    for equip in equip_info:
+        my_equips[equip['id']] = {
+            'id': equip['id'],
+            'name': stc_to_text(equip_text, equip["name"]),
+            'fit_guns': equip['fit_guns'],
+            'level_00': 0,
+            'level_10': 0,
+        }
+
+    for _, equip in user_info['equip_with_user_info'].items():
+        if equip['equip_id'] not in my_equips.keys():
+            continue
+        level = int(equip['equip_level'])
+        if level == 10:
+            my_equips[equip['equip_id']]['level_10'] += 1
+        else:
+            my_equips[equip['equip_id']]['level_00'] += 1
+
+    return {"my_dolls": my_dolls, "my_equips": my_equips}
+
+
+def main():
+    gun_info = open_json(r'resource/gun_info.json', "utf-8")
+    equip_info = open_json(r'resource/equip_info.json', "utf-8")
+    equip_text = open_text(r'resource/equip.txt')
+    my_dolls = user_info_handle()["my_dolls"]
+    my_equips = user_info_handle()["my_equips"]
+
+    # %% 计算各人形不同配装的效能
+    choices = {}
+    for gun in gun_info:
+        gun_id = gun['id']
+        if 1200 < int(gun_id) < 20000 or int(gun_id) > 30000:
+            continue
+        if my_dolls[gun_id]['gun_level'] == 0:
+            continue
+
+        equip_group_all = []
+        for num in ["1", "2", "3"]:
+            equip_group_category = []
+            types = gun[f'type_equip{num}'].split(";")[-1].split(',')
+            for equip in equip_info:
+                if equip['type'] not in types:
+                    continue
+                if equip['fit_guns'] and gun_id not in equip['fit_guns'].split(','):
+                    continue
+                eid = equip['id']
+                if my_equips[eid]['level_10'] > 0:
+                    equip_group_category.append((equip, 10))
+                if my_equips[eid]['level_00'] > 0 and my_equips[eid]['level_10'] < MAX_DOLL:
+                    equip_group_category.append((equip, 0))
+            equip_group_all.append(equip_group_category)
+
+        for i, j, k in itertools.product(*equip_group_all):
+            strength = doll_attr_calculate(gun, my_dolls[gun_id], [i, j, k])
+            # print(strength)
+            sp_ratio = 1.2 if gun_id in ADVANTAGE else 1
+            score = math.floor(int(COEF[TYPE[int(gun['type'])]]) * sp_ratio * FAIRY * strength[ENVIRONMENT] / 100)
+            # print(name_table[i[0]['name']],i[1],name_table[j[0]['name']],j[1],name_table[k[0]['name']],k[1],score)
+            recipe_name = f"{my_dolls[gun_id]['name']}_" \
+                          f"{stc_to_text(equip_text, i[0]['name'])}_{i[1]}_" \
+                          f"{stc_to_text(equip_text, j[0]['name'])}_{j[1]}_" \
+                          f"{stc_to_text(equip_text, k[0]['name'])}_{k[1]}"
+            recipe_content = {
+                my_dolls[gun_id]['name']: -1,
+                f"{stc_to_text(equip_text, i[0]['name'])}_{i[1]}": 0,
+                f"{stc_to_text(equip_text, j[0]['name'])}_{j[1]}": 0,
+                f"{stc_to_text(equip_text, k[0]['name'])}_{k[1]}": 0,
+                'score': score
+            }
+            recipe_content[f"{stc_to_text(equip_text, i[0]['name'])}_{i[1]}"] -= 1
+            recipe_content[f"{stc_to_text(equip_text, j[0]['name'])}_{j[1]}"] -= 1
+            recipe_content[f"{stc_to_text(equip_text, k[0]['name'])}_{k[1]}"] -= 1
+            choices[recipe_name] = recipe_content
+    print(len(choices.items()))
+
+    # %%
+    resource = {}
+    for id, doll in my_dolls.items():
+        if doll['gun_level'] > 0:
+            resource[doll['name']] = 1
+    for id, equip in my_equips.items():
+        if equip['level_10'] > 0:
+            resource[f"{equip['name']}_10"] = equip['level_10']
+        if equip['level_00'] > 0:
+            resource[f"{equip['name']}_0"] = equip['level_00']
+    resource['count'] = MAX_DOLL
+    resource['score'] = 0
+    lp_vars = {}
+    problem = lp.LpProblem('battlefield', lp.LpMaximize)
+    for k, recipe in choices.items():
+        lp_vars[k] = lp.LpVariable(k, cat=lp.LpBinary)
+        for r, c in recipe.items():
+            resource[r] += c * lp_vars[k]
+        resource['count'] -= lp_vars[k]
+    for k, v in resource.items():
+        problem += v >= 0, k
+    problem += resource['score']
+    print(problem.solve())
+    print(resource['score'].value())
+    res = []
+    for k, v in lp_vars.items():
+        if v.value() > 0:
+            res.append((k, choices[k]['score'], v.value()))
+    res.sort(key=lambda x: x[1], reverse=True)
+    for r in res:
+        print(r[0], r[1], r[2])
+    # %%
+
+
+theater_area_setting("748")
+print(user_info_handle())
+main()
--- utils.py
+++ utils.py
@@ -1,55 +1,51 @@
 
 import csv
 import math
-# %%
-def get_name_table():
-    with open(r'resource/table.tsv','r',encoding='utf-8') as f:
-        name_table = {}
-        for row in csv.DictReader(f, delimiter='\t'):
-            name_table[row['key']] = row['zh-CN']
-    return name_table
-# %%
+import ujson
 
-def doll_attr_calculate(doll, my_doll, equip_group):
+
+def doll_attr_calculate(gun, my_doll, equip_group):
     lv = my_doll['gun_level']
-    favor_factor = 0.95 + (my_doll['favor']+10)//50*0.05
+    favor_factor = 0.95 + (my_doll['favor'] + 10) // 50 * 0.05
 
-    attr_change = {"hp": 0, "pow": 0, "rate": 0, "hit": 0, "dodge": 0, "armor": 0}
-    attr_fixed = {"critical_harm_rate": 150, "critical_percent": doll['crit'],
-                  "armor_piercing": doll['armor_piercing'], "night_view_percent": 0, "bullet": doll['bullet']}
-    attr_other = {
-        "id": doll["id"], "star": doll["rank"], "upgrade": lv, "type": doll["type"], 
-        "skill_effect_per": 0, "skill_effect": 0, 
-        'number': my_doll['number'], 'skill1':my_doll['skill1'], 'skill2':my_doll['skill2']}
+    attr_change = {"life": 0, "pow": 0, "rate": 0, "hit": 0, "dodge": 0, "armor": 0}
+    attr_fixed = {"critical_harm_rate": 150, "critical_percent": gun['crit'],
+                  "armor_piercing": gun['armor_piercing'], "night_view_percent": 0, "bullet": gun['special']}
+    attr_other = {"id": gun["id"], "star": gun["rank"], "upgrade": lv, "type": gun["type"], "skill_effect_per": 0,
+                  "skill_effect": 0, 'number': my_doll['number'], 'skill1': my_doll['skill1'], 'skill2': my_doll['skill2']}
 
     for key in ["pow", "hit", "dodge"]:
-        attr_change[key] = gf_ceil(calculate(lv, key, doll) * favor_factor)
-    for key in ["hp", "rate", "armor"]:
-        attr_change[key] = gf_ceil(calculate(lv, key, doll))
+        attr_change[key] = gf_ceil(calculate(lv, key, gun) * favor_factor)
+    for key in ["life", "rate", "armor"]:
+        attr_change[key] = gf_ceil(calculate(lv, key, gun))
 
     for equip, elv in equip_group:
         if not equip:
             continue
-        attr_other["skill_effect_per"] = 30 if equip['id']==117 or equip['id']==118 else 0
+
+        attr_other["skill_effect_per"] += int(equip["skill_effect_per"])
         attr_other["skill_effect"] += int(equip["skill_effect"])
 
-        for attr in attr_change.keys():
-            if attr in equip['stat'].keys():
-                stat = equip['stat'][attr]
-                modif = 1
-                if 'upgrade' in stat.keys() and elv == 10:
-                    modif += stat['upgrade']/1000
-                value = math.floor(stat['max'] * modif)
-                attr_change[attr] += value
+        equip_mul = {}
+        if equip['bonus_type']:
+            equip_mul = bonus_handle(equip['bonus_type'])
 
-        for attr in attr_fixed.keys():
-            if attr in equip['stat'].keys():
-                stat = equip['stat'][attr]
-                modif = 1
-                if 'upgrade' in stat.keys() and elv == 10:
-                    modif += stat['upgrade']/1000
-                value = math.floor(stat['max'] * modif)
-                attr_fixed[attr] += value
+        for key in attr_change.keys():
+            if key in equip.keys() and equip[key] and (key not in equip_mul.keys()):
+                equip_mul[key] = "1"
+        for key in attr_fixed.keys():
+            if key in equip.keys() and equip[key] and (key not in equip_mul.keys()):
+                equip_mul[key] = "1"
+
+        # print(equip_mul)
+        for key in equip_mul.keys():
+            attr = math.floor(float(equip[key].split(",")[-1]) * float(equip_mul[key]))
+
+            if key in attr_change.keys():
+                attr_change[key] = int(attr_change[key]) + int(attr)
+            elif key in attr_fixed.keys():
+                attr_fixed[key] = int(attr_fixed[key]) + int(attr)
+
     day = doll_effect_calculate({"attr_change": attr_change, "attr_fixed": attr_fixed, "attr_other": attr_other}, "day")
     night = doll_effect_calculate({"attr_change": attr_change, "attr_fixed": attr_fixed, "attr_other": attr_other}, "night")
 
@@ -61,20 +57,21 @@ def doll_effect_calculate(gun_attr, fight_type):
     skill1 = gun_attr["attr_other"]['skill1']
     skill2 = gun_attr["attr_other"]['skill2']
     star = int(gun_attr["attr_other"]["star"])
+    number = gun_attr["attr_other"]["number"]
     skill_effect = int(gun_attr["attr_other"]["skill_effect"])
     skill_effect_per = int(gun_attr["attr_other"]["skill_effect_per"])
 
     # 1技能效能 = ceiling（5*(0.8+星级/10)*[35+5*(技能等级-1)]*(100+skill_effect_per)/100,1) + skill_effect
     # 2技能效能 = ceiling（5*(0.8+星级/10)*[15+2*(技能等级-1)]*(100+skill_effect_per)/100,1) + skill_effect
-    doll_skill_effect = gf_ceil(5*(0.8+star/10)*(35+5*(skill1-1))*(100+skill_effect_per)/100) + skill_effect
+    doll_skill_effect = gf_ceil(number*(0.8+star/10)*(35+5*(skill1-1))*(100+skill_effect_per)/100) + skill_effect
     if gun_attr["attr_other"]["upgrade"] >= 110:
-        doll_skill_effect += gf_ceil(5*(0.8+star/10)*(15+2*(skill2-1))*(100+skill_effect_per)/100)
+        doll_skill_effect += gf_ceil(number*(0.8+star/10)*(15+2*(skill2-1))*(100+skill_effect_per)/100)
 
-    life = int(gun_attr["attr_change"]["hp"])
+    life = int(gun_attr["attr_change"]["life"])
     dodge = int(gun_attr["attr_change"]["dodge"])
     armor = int(gun_attr["attr_change"]["armor"])
     # 防御效能 = CEILING(生命*(35+闪避)/35*(4.2*100/MAX(1,100-护甲)-3.2),1)
-    defend_effect = gf_ceil(life*5*(35+dodge)/35*(4.2*100/max(1, 100-armor)-3.2))
+    defend_effect = gf_ceil(life*number*(35+dodge)/35*(4.2*100/max(1, 100-armor)-3.2))
 
     hit = int(gun_attr["attr_change"]["hit"])
     night_view_percent = int(gun_attr["attr_fixed"]["night_view_percent"])
@@ -90,13 +87,13 @@ def doll_effect_calculate(gun_attr, fight_type):
     bullet = int(gun_attr["attr_fixed"]["bullet"])
     if gun_attr["attr_other"]["type"] == "SG":
         # SG攻击 = 6*5*(3*弹量*(伤害+穿甲/3)*(1+暴击率*(暴击伤害-100)/10000)/(1.5+弹量*50/射速+0.5*弹量)*命中/(命中+23)+8)
-        attack_effect = gf_ceil(6*5*(3*bullet*(attack+armor_piercing/3)*(1+critical*(critical_damage-100)/10000)/(1.5+bullet*50/rate+0.5*bullet)*hit/(hit+23)+8))
+        attack_effect = gf_ceil(6*number*(3*bullet*(attack+armor_piercing/3)*(1+critical*(critical_damage-100)/10000)/(1.5+bullet*50/rate+0.5*bullet)*hit/(hit+23)+8))
     elif gun_attr["attr_other"]["type"] == "MG":
         # MG攻击 = 7*5*(弹量*(伤害+穿甲/3)*(1+暴击率*(暴击伤害-100)/10000)/(弹量/3+4+200/射速)*命中/(命中+23)+8)
-        attack_effect = gf_ceil(7*5*(bullet*(attack+armor_piercing/3)*(1+critical*(critical_damage-100)/10000)/(bullet/3+4+200/rate)*hit/(hit+23)+8))
+        attack_effect = gf_ceil(7*number*(bullet*(attack+armor_piercing/3)*(1+critical*(critical_damage-100)/10000)/(bullet/3+4+200/rate)*hit/(hit+23)+8))
     else:
         # 其他攻击 = 5*5*(伤害+穿甲/3)*(1+暴击率*(暴击伤害-100)/10000)*射速/50*命中/(命中+23)+8)
-        attack_effect = gf_ceil(5*5*((attack+armor_piercing/3)*(1+critical*(critical_damage-100)/10000)*rate/50*hit/(hit+23)+8))
+        attack_effect = gf_ceil(5*number*((attack+armor_piercing/3)*(1+critical*(critical_damage-100)/10000)*rate/50*hit/(hit+23)+8))
 
     effect_total = doll_skill_effect + defend_effect + attack_effect
     return effect_total
@@ -106,6 +103,26 @@ def stc_to_text(text, name):
     tem = text[text.find(name) + len(name) + 1:]
     out_text = tem[:tem.find("\n")]
     return out_text
+
+
+def open_json(path, encoding="none"):
+    if encoding == "utf-8":
+        with open(path, encoding='utf-8') as f:
+            info = ujson.load(f)
+            f.close()
+    else:
+        with open(path) as f:
+            info = ujson.load(f)
+            f.close()
+
+    return info
+
+
+def open_text(path):
+    with open(path, encoding='utf-8') as f:
+        text = f.read()
+        f.close()
+    return text
 
 
 def bonus_handle(string):
@@ -125,6 +142,7 @@ def gf_ceil(number):
         number = number - (number % 1) + 1
     return int(number)
 
+
 BASIC = [16, 45, 5, 5]
 BASIC_LIFE_ARMOR = [
     [[55, 0.555], [2, 0.161]],
@@ -143,17 +161,18 @@ GROW = [
     [[0.06, 18.018], [0.022, 15.741], [0.075, 22.572], [0.075, 22.572]]
 ]
 TYPE_ENUM = {"HG": 0, "SMG": 1, "RF": 2, "AR": 3, "MG": 4, "SG": 5}
-ATTR_ENUM = {"hp": 0, "pow": 1, "rate": 2, "hit": 3, "dodge": 4, "armor": 5}
+ATTR_ENUM = {"life": 0, "pow": 1, "rate": 2, "hit": 3, "dodge": 4, "armor": 5}
 
 
-def calculate(lv, attr_type, doll):
+def calculate(lv, attr_type, gun):
     mod = 1
     if lv <= 100:
         mod = 0
-    guntype = TYPE_ENUM[doll['type']]
+
+    guntype = int(gun['type']) - 1
     attr = ATTR_ENUM[attr_type]
-    ratio = doll['stat'][attr_type]
-    growth = doll['grow']
+    ratio = int(gun['ratio_' + attr_type])
+    growth = int(gun['eat_ratio'])
 
     if attr == 0 or attr == 5:
         return math.ceil(
